eda9fcda7ef00493668fbd03719cf6b30def7d95
haru3613 <qaw8975889@gmail.com>
2018-10-09 15:22:21 +0800

    1009

--- app/src/main/java/tw/edu/cyut/englishapp/model/ItemTestSpeak.java
+++ app/src/main/java/tw/edu/cyut/englishapp/model/ItemTestSpeak.java
@@ -4,27 +4,39 @@ import com.google.gson.annotations.SerializedName;
 
 //Topic_speak
 public class ItemTestSpeak {
-    @SerializedName("tid")
-    private String tid;
-    @SerializedName("qorder")
-    private String qorder;
-    @SerializedName("eid")
-    private String eid;
+    @SerializedName("aid")
+    private String aid;
     @SerializedName("uid")
     private String uid;
-    @SerializedName("ans")
-    private String ans;
+    @SerializedName("eid")
+    private String eid;
+    @SerializedName("tid")
+    private String tid;
+    @SerializedName("mode")
+    private String mode;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("mime")
+    private String mime;
+    @SerializedName("audio")
+    private String audio;
+    @SerializedName("reviews")
+    private String reviews;
     @SerializedName("created_at")
     private String created_at;
     @SerializedName("updated_at")
     private String updated_at;
 
-    public ItemTestSpeak(String tid, String uid , String eid, String qorder, String ans, String created_at, String updated_at){
+    public ItemTestSpeak(String aid,String tid, String uid , String eid, String mode, String name,String mime,String audio,String reviews, String created_at, String updated_at){
         this.tid=tid;
         this.uid=uid;
         this.eid=eid;
-        this.qorder=qorder;
-        this.ans=ans;
+        this.aid=aid;
+        this.mode=mode;
+        this.name=name;
+        this.mime=mime;
+        this.audio=audio;
+        this.reviews=reviews;
         this.created_at=created_at;
         this.updated_at=updated_at;
     }
@@ -33,9 +45,6 @@ public class ItemTestSpeak {
         return tid;
     }
 
-    public String getQorder() {
-        return qorder;
-    }
 
     public String getEid() {
         return eid;
@@ -45,9 +54,6 @@ public class ItemTestSpeak {
         return uid;
     }
 
-    public String getAns() {
-        return ans;
-    }
 
     public String getCreated_at() {
         return created_at;
@@ -55,5 +61,29 @@ public class ItemTestSpeak {
 
     public String getUpdated_at() {
         return updated_at;
+    }
+
+    public String getAid() {
+        return aid;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMime() {
+        return mime;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public String getReviews() {
+        return reviews;
     }
 }
