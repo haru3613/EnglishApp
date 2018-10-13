98ce012889b00825091afea7dead367bf5726530
haru3613 <qaw8975889@gmail.com>
2018-10-13 18:05:19 +0800

    pre-test

---
+++ app/src/main/java/tw/edu/cyut/englishapp/PreStartActivity.java
@@ -0,0 +1,225 @@
+package tw.edu.cyut.englishapp;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.bumptech.glide.Glide;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.android.volley.VolleyLog.TAG;
+import static tw.edu.cyut.englishapp.LoginActivity.KEY;
+
+
+public class PreStartActivity extends Activity {
+
+    private ImageButton play,ans1,ans2,ans3,ans4,next;
+    private String correct_ans,uid, choice_ans,day,file_name;
+    private  Boolean isExit = false;
+    private  Boolean hasTask = false;
+    private boolean playPause;
+    private MediaPlayer mediaPlayer;
+    private ProgressDialog progressDialog;
+    private String[][] audio_list=new String[16][105];
+    private int play_count;
+    private boolean initialStage = true;
+    Timer timerExit = new Timer();
+    TimerTask task = new TimerTask() {
+
+        @Override
+        public void run() {
+            isExit = false;
+            hasTask = true;
+        }
+    };
+
+    //按兩次Back退出app
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // 判斷是否按下Back
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // 是否要退出
+            if(!isExit ) {
+                isExit = true; //記錄下一次要退出
+                Toast.makeText(this, "Press Back again to exit the app."
+                        , Toast.LENGTH_SHORT).show();
+                // 如果超過兩秒則恢復預設值
+                if(!hasTask) {
+                    timerExit.schedule(task, 2000);
+                }
+            } else {
+                finish(); // 離開程式
+                System.exit(0);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+
+        //TODO 初始化元件
+        initPreStartActivity();
+
+        final String origin_time=getDateNow();
+        Log.d("onCreate", "TimeNow:"+origin_time);
+
+        //紀錄播放幾次
+        play_count=0;
+        uid="";
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(KEY, Context.MODE_PRIVATE);
+        uid=sharedPreferences.getString("uid",null);
+        Log.d("onCreate", "uid: "+uid);
+
+        Intent intent = this.getIntent();//取得傳遞過來的資料
+        final String t_index = intent.getStringExtra("index");
+
+        Log.d("onCreate","這是第幾題:"+t_index);
+
+
+        //接收array
+        audio_list=null;
+        Object[] objectArray = (Object[]) getIntent().getExtras().getSerializable("audio_list");
+        if(objectArray!=null){
+            audio_list = new String[objectArray.length][];
+            Log.d(TAG, "onCreate: length"+objectArray.length);
+            for(int i=0;i<objectArray.length;i++){
+                audio_list[i]=(String[]) objectArray[i];
+            }
+        }
+
+        Log.d(TAG, "onCreate: 音檔名稱"+audio_list[0][Integer.parseInt(t_index)]);
+
+        String origin=audio_list[Integer.parseInt(day)][Integer.parseInt(t_index)];
+        char c=(audio_list[Integer.parseInt(day)][Integer.parseInt(t_index)]).charAt(-3);
+        int index=origin.indexOf(c);
+        Log.d("","題目名稱:"+origin.substring(0,index)+"答案:"+c);
+
+
+        choice_ans="";
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        progressDialog = new ProgressDialog(this);
+
+        //TODO 下一題時update index
+
+
+    }
+
+
+    private void initPreStartActivity(){
+
+    }
+
+
+    private String getDateNow(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date curDate = new Date(System.currentTimeMillis()) ; // 獲取當前時間
+        return formatter.format(curDate);
+    }
+
+
+    private boolean date_count(String origin ,String now){
+        Log.d(TAG, "date_count: origin:"+origin);
+        Log.d(TAG, "date_count: now:"+now);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        try {
+            Date dt1 =sdf.parse(origin);
+            Date dt2 =sdf.parse(now);
+            Long ut1=dt1.getTime();
+            Long ut2=dt2.getTime();
+            Long timeP=ut2-ut1;
+            Long min=timeP/1000*60;
+            if (min>3){
+                return true;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+       return false;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+
+    class Player extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            Boolean prepared = false;
+
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        initialStage = true;
+                        playPause = false;
+                        //btn.setText("Launch Streaming");
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                });
+
+                mediaPlayer.prepare();
+                prepared = true;
+
+            } catch (Exception e) {
+                Log.e("MyAudioStreamingApp", e.getMessage());
+                prepared = false;
+            }
+
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+
+            if (progressDialog.isShowing()) {
+                progressDialog.cancel();
+            }
+
+            mediaPlayer.start();
+            initialStage = false;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog.setMessage("Buffering...");
+            progressDialog.show();
+        }
+    }
+}
--- app/src/main/java/tw/edu/cyut/englishapp/PreTestActivity.java
+++ app/src/main/java/tw/edu/cyut/englishapp/PreTestActivity.java
@@ -1,38 +1,83 @@
 package tw.edu.cyut.englishapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import tw.edu.cyut.englishapp.Group.TestGroupActivity;
+import tw.edu.cyut.englishapp.model.ItemTopicSpeak;
 
 import static tw.edu.cyut.englishapp.LoginActivity.KEY;
 
 public class PreTestActivity extends Activity {
-
+    private String index;
+    private String [][] pre_test_array=new String[16][105];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_test);
 
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(KEY, Context.MODE_PRIVATE);
-        String uid=sharedPreferences.getString("uid",null);
+        Button btn_start=findViewById(R.id.btn_pre_start);
 
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(KEY, Context.MODE_PRIVATE);
+        final String uid=sharedPreferences.getString("uid",null);
+
+        //讀取第幾題
+        LoadTopicSpeak(uid);
+
+        //xml to array
+        int j=0;
+        for (TypedArray item : ResourceHelper.getMultiTypedArray(PreTestActivity.this, "day")) {
+            for (int i=1;i<=Integer.parseInt(item.getString(0));i++){
+                pre_test_array[j][i]=item.getString(i);
+            }
+            j++;
+        }
 
         AlertDialog("Pre-test","You are about to start the pretest. Please make sure your microphone is functioning properly and carefully follow the instructions listed below:");
 
-
-        //TODO if finish pre-test then update user day=1
-        String type = "Update user day";
-        Backgorundwork backgorundwork = new Backgorundwork(PreTestActivity.this);
-        backgorundwork.execute(type,uid,"1");
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenPreStartActivity(index);
+            }
+        });
     }
 
+    private void OpenPreStartActivity(String t_index){
+        Intent ToAnswer=new Intent(PreTestActivity.this,PreStartActivity.class);
+        Bundle mBundle = new Bundle();
+        ToAnswer.putExtra("index", t_index);
+        mBundle.putSerializable("audio_list", pre_test_array);
+        ToAnswer.putExtras(mBundle);
+        startActivity(ToAnswer);
+        finish();
+    }
     private void AlertDialog(String title,String content){
         boolean wrapInScrollView = true;
         MaterialDialog dialog=new MaterialDialog.Builder(PreTestActivity.this)
@@ -45,5 +90,48 @@ public class PreTestActivity extends Activity {
         TextView content_txt=item.findViewById(R.id.dialog_content);
         content_txt.setText(content);
         dialog.show();
+    }
+
+    public void LoadTopicSpeak(final String uid){
+        String url = "http://140.122.63.99/app/load_topic_speak.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response:",response);
+                        try {
+                            byte[] u = response.getBytes(
+                                    "UTF-8");
+                            response = new String(u, "UTF-8");
+                            Log.d(ContentValues.TAG, "Response " + response);
+                            GsonBuilder builder = new GsonBuilder();
+                            Gson mGson = builder.create();
+                            List<ItemTopicSpeak> posts = new ArrayList<ItemTopicSpeak>();
+                            posts = Arrays.asList(mGson.fromJson(response, ItemTopicSpeak[].class));
+                            index=posts.get(0).getTopic_index();
+
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //do stuffs with response erro
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("uid",uid);
+
+                return params;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(PreTestActivity.this);
+        requestQueue.add(stringRequest);
     }
 }
