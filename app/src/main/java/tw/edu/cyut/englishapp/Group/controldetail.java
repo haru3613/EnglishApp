c4eef79295a0a514572791172a3ac7450c2f0a22
KaiAiYu <robinzhang@exosite.com>
2018-10-15 01:28:11 +0800

    I wanna cry

--- app/src/main/java/tw/edu/cyut/englishapp/Group/controldetail.java
+++ app/src/main/java/tw/edu/cyut/englishapp/Group/controldetail.java
@@ -1,8 +1,12 @@
 package tw.edu.cyut.englishapp.Group;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import tw.edu.cyut.englishapp.LoginActivity;
 import tw.edu.cyut.englishapp.R;
 
 public class controldetail extends AppCompatActivity {
@@ -11,5 +15,21 @@ public class controldetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controldetail);
+    }
+
+    public void bt_continue(View view) {
+        Intent i = new Intent();
+        i.setClass(controldetail.this, LoginActivity.class);
+        startActivity(i);
+        controldetail.this.finish();//結束目前 Activity
+
+    }
+
+    public void bt_back(View view) {
+        Intent i = new Intent();
+        i.setClass(controldetail.this, ControlPreTestActivity.class);
+        startActivity(i);
+        controldetail.this.finish();//結束目前 Activity
+
     }
 }
